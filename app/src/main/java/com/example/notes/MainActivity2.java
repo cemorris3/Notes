acc54f993758c6d4b0f0539ce1ffdada4a8ec30b
caitlinmorris <cemorris3@wisc.edu>
2021-10-16 17:12:17 -0500

    through building third screen

--- app/src/main/java/com/example/notes/MainActivity2.java
+++ app/src/main/java/com/example/notes/MainActivity2.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -29,15 +33,43 @@ public class MainActivity2 extends AppCompatActivity {
         welcome = findViewById(R.id.welcome);
 //        Intent intent = getIntent();
 //        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // get welcome message
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
         String message = sharedPreferences.getString(userKey,"");
-
+        // display the message
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 welcome.setText("Welcome " + message + "!");
             }
         });
+        // get SQLiteDatabase instance
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes",Context.MODE_PRIVATE,null);
+
+        DBHelper helper = new DBHelper(sqLiteDatabase);
+        notes = helper.readNotes(message);
+
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes){
+            displayNotes.add(String.format("Title:%s\nDate:%s\n", note.getTitle(),note.getDate()));
+        }
+
+        // use listview to display them on screen
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,displayNotes);
+        ListView listView = (ListView) findViewById(R.id.noteList);
+        listView.setAdapter(adapter);
+
+        // add onItemClickListener for ListView item, a note
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),Note.class);
+                intent.putExtra("noteid",position);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -50,6 +82,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         super.onOptionsItemSelected(item);
+
         if(item.getItemId() == R.id.logout){
             //erase username from shared preferences
             Intent intent = new Intent(this, MainActivity.class);
@@ -65,4 +98,6 @@ public class MainActivity2 extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
--- app/src/main/java/com/example/notes/MainActivity3.java
+++ app/src/main/java/com/example/notes/MainActivity3.java
@@ -7,10 +7,14 @@ import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    int noteid = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
     }
 
     public void savedNote(View view){
